b6dcca67b5d3432c64edf9f5c0a0b158af9db270
Michael Nowak <nowakmikea@gmail.com>
2023-02-04 00:24:55 -0600

    Added tag sorting for tag sidebar

--- lib/components/tag_sidebar.dart
+++ lib/components/tag_sidebar.dart
@@ -10,39 +10,49 @@ class TagSidebar extends StatefulWidget {
   State<TagSidebar> createState() => _TagSidebarState();
 }
 
-enum _TagSort {
-  count,
-  alphabetical
-}
+enum _TagSort { count, alphabetical }
 
 class _TagSidebarState extends State<TagSidebar> {
   _TagSort sort = _TagSort.count;
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<List<TagCount>>(
-      stream: widget.stream,
-      builder: (context, snapshot) {
-        if (!snapshot.hasData || snapshot.data!.isEmpty) {
-          return const Center(child: Text('No tags found'));
-        } else {
-          sort == _TagSort.count
-            ? snapshot.data!.sort((l, r) {
-              final countCompare = l.count.compareTo(r.count) * -1;
+    return Column(children: [
+      Flexible(
+        child: Row(
+          children: [
+            IconButton(onPressed: () => setState(() {sort = _TagSort.count;}), icon: const Icon(Icons.sort)),
+            IconButton(onPressed: () => setState(() {sort = _TagSort.alphabetical; }), icon: const Icon(Icons.sort_by_alpha)),
+          ],
+        ),
+      ),
+      Expanded(
+        child: StreamBuilder<List<TagCount>>(
+          stream: widget.stream,
+          builder: (context, snapshot) {
+            if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return const Center(child: Text('No tags found'));
+            } else {
+              sort == _TagSort.count
+                  ? snapshot.data!.sort((l, r) {
+                      final countCompare = l.count.compareTo(r.count) * -1;
 
-              return countCompare == 0 ? l.tag.compareTo(r.tag) : countCompare;
-            })
-            : snapshot.data!.sort((l, r) => l.tag.compareTo(r.tag));
-          return ListView.builder(
-            itemCount: snapshot.data!.length,
-            itemBuilder: (context, idx) {
-              return Text(
-                  '${snapshot.data![idx].tag} (${snapshot.data![idx].count})');
-            },
-          );
-        }
-      },
-    );
+                      return countCompare == 0
+                          ? l.tag.compareTo(r.tag)
+                          : countCompare;
+                    })
+                  : snapshot.data!.sort((l, r) => l.tag.compareTo(r.tag));
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, idx) {
+                  return Text(
+                      '${snapshot.data![idx].tag} (${snapshot.data![idx].count})');
+                },
+              );
+            }
+          },
+        ),
+      ),
+    ]);
   }
-
 }
