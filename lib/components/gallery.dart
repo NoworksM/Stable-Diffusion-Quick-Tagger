e47f8c4c8b85b26435d3e2421dad91f230e17b7f
Michael Nowak <nowakmikea@gmail.com>
2023-03-23 19:57:43 -0500

    Updated display of image_count_footer.dart to use the tabs image counts

--- lib/components/gallery.dart
+++ lib/components/gallery.dart
@@ -1,51 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:quick_tagger/components/gallery_image.dart';
 import 'package:quick_tagger/data/tagged_image.dart';
-import 'package:quick_tagger/ioc.dart';
 import 'package:quick_tagger/pages/tag_editor_page.dart';
-import 'package:quick_tagger/services/gallery_service.dart';
-import 'package:quick_tagger/utils/tag_utils.dart' as tag_utils;
 
 class Gallery extends StatelessWidget {
-  final List<TaggedImage>? initialImages;
-  final Stream<List<TaggedImage>> stream;
+  final List<TaggedImage> images;
   final String? hoveredTag;
   final Set<String>? selectedImages;
-  final Set<String> includedTags;
-  final Set<String> excludedTags;
   final Function(TaggedImage)? onImageSelected;
-  final _galleryService = getIt.get<IGalleryService>();
 
-  Gallery({super.key, this.initialImages, required this.stream, required this.includedTags, required this.excludedTags, this.hoveredTag, this.selectedImages, this.onImageSelected});
+  const Gallery({super.key, required this.images, this.hoveredTag, this.selectedImages, this.onImageSelected});
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      initialData: initialImages,
-      stream: stream,
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return const Center(child: CircularProgressIndicator());
-        } else {
-          final images = tag_utils.filterImagesForTagsAndEdits(snapshot.data!, _galleryService.pendingEdits, includedTags, excludedTags);
-
-          return GridView.builder(
-            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 300,
-              childAspectRatio: 1,
-              crossAxisSpacing: 0,
-              mainAxisSpacing: 0,
-            ),
-            itemCount: images.length,
-            itemBuilder: (ctx, idx) => GalleryImage(
-                image: images[idx],
-                hoveredTag: hoveredTag,
-                selected: selectedImages?.contains(images[idx].path) ?? false,
-                onSelected: () => onImageSelected?.call(images[idx]),
-                onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => TagEditorPage(initialIndex: idx, images: images)))),
-          );
-        }
-      },
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 300,
+        childAspectRatio: 1,
+        crossAxisSpacing: 0,
+        mainAxisSpacing: 0,
+      ),
+      itemCount: images.length,
+      itemBuilder: (ctx, idx) => GalleryImage(
+          image: images[idx],
+          hoveredTag: hoveredTag,
+          selected: selectedImages?.contains(images[idx].path) ?? false,
+          onSelected: () => onImageSelected?.call(images[idx]),
+          onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => TagEditorPage(initialIndex: idx, images: images)))),
     );
   }
 }
--- lib/data/gallery_tab.dart
+++ lib/data/gallery_tab.dart
@@ -2,31 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:quick_tagger/components/gallery.dart';
 import 'package:quick_tagger/components/image_count_footer.dart';
 import 'package:quick_tagger/data/tagged_image.dart';
+import 'package:quick_tagger/ioc.dart';
+import 'package:quick_tagger/services/gallery_service.dart';
+import 'package:quick_tagger/utils/tag_utils.dart' as tag_utils;
 
 class GalleryTab extends StatelessWidget {
   final List<TaggedImage>? initialImages;
   final Stream<List<TaggedImage>> imageStream;
   final String? hoveredTag;
   final Set<String> selectedImagePaths;
-  final int imageCount;
-  final int filteredImageCount;
-  final int selectedImageCount;
-  final int filteredTagCount;
   final int tagCount;
   final Set<String> includedTags;
   final Set<String> excludedTags;
   final Function(TaggedImage)? onImageSelected;
   final Function()? onClearSelection;
+  final _galleryService = getIt.get<IGalleryService>();
 
-  const GalleryTab({super.key,
+  GalleryTab({super.key,
     this.initialImages,
     required this.imageStream,
     this.hoveredTag,
     required this.selectedImagePaths,
-    required this.imageCount,
-    required this.filteredImageCount,
-    required this.selectedImageCount,
-    required this.filteredTagCount,
     required this.tagCount,
     required this.includedTags,
     required this.excludedTags,
@@ -35,31 +31,42 @@ class GalleryTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Expanded(
-          child: Gallery(
-              initialImages: initialImages,
-              stream: imageStream,
-              hoveredTag: hoveredTag,
-              selectedImages: selectedImagePaths,
-              includedTags: includedTags,
-              excludedTags: excludedTags,
-              onImageSelected: (i) => onImageSelected?.call(i)),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: ImageCountFooter(
-            images: imageCount,
-            filtered: filteredImageCount,
-            selected: selectedImageCount,
-            filteredTags: filteredTagCount,
-            totalTags: tagCount,
-            onClearSelection: () => onClearSelection?.call(),
-          ),
-        ),
-      ],
+    return StreamBuilder<List<TaggedImage>>(
+      initialData: initialImages,
+      stream: imageStream,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData){
+          return const Center(child: CircularProgressIndicator());
+        } else {
+          final images = tag_utils.filterImagesForTagsAndEdits(snapshot.data!, _galleryService.pendingEdits, includedTags, excludedTags);
+
+          final selected = images.where((i) => selectedImagePaths.contains(i.path)).length;
+
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Gallery(
+                    images: images,
+                    hoveredTag: hoveredTag,
+                    selectedImages: selectedImagePaths,
+                    onImageSelected: (i) => onImageSelected?.call(i)),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: ImageCountFooter(
+                  images: snapshot.data!.length,
+                  filtered: images.length,
+                  selected: selected,
+                  filteredTags: includedTags.length + excludedTags.length,
+                  totalTags: tagCount,
+                  onClearSelection: () => onClearSelection?.call(),
+                ),
+              ),
+            ],
+          );
+        }
+      }
     );
   }
 }
